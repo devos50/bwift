df9d1858d655b631deaaa17a1863d701c846a0d2
Martijn de Vos <mdmartijndevos@gmail.com>
2017-08-17 21:42:03 +0200

    Supported multiple instances

--- bwift/app/repository/NodeRepository.java
+++ bwift/app/repository/NodeRepository.java
@@ -57,7 +57,7 @@ public class NodeRepository {
             // create some banks
             if(Node.find.all().size() == 0) {
                 instance.addNode("Maersk", true, "", "NL11NORD111111111", "127.0.0.1", 9000);
-                instance.addNode("Nordea", false, "NORD", "", "127.0.0.1", 9000);
+                instance.addNode("Nordea", false, "NORD", "", "127.0.0.1", 9001);
                 instance.addNode("Company", true, "", "NL11NORD111111111", "127.0.0.1", 9000);
             }
         }
